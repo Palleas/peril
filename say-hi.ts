9adf646a3325469996459692907b82ba80127eaa
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-22 11:32:08 -0700

    Use `warn` as well

--- say-hi.ts
+++ say-hi.ts
@@ -1,7 +1,8 @@
-import { message, danger } from "danger"
+import { message, warn, danger } from "danger"
 
 export default async () => {
     const issue = danger.github.issue;
     console.log('Dealing with an issue here', { issue });
     message("Hi!");
+    warn('Uh-oh, that is not good');
 }
