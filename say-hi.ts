ac624a0b3260918af40e8c0bac80eafebc1db069
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-22 11:47:30 -0700

    Fix settings

--- say-hi.ts
+++ say-hi.ts
@@ -1,8 +1,9 @@
-import { message, warn, danger } from "danger"
+import { message, warn, danger, markdown } from "danger"
 
 export default async () => {
     const issue = danger.github.issue;
     console.log('Dealing with an issue here', { issue });
     message("Hi!");
     warn('Uh-oh, that is not good');
+    markdown('# This does not work, does it?');
 }
