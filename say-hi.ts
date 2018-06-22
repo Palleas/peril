6c1b9a2c89e83e659569977aa1f7538c4ec20b35
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-22 13:09:23 -0700

    Add label

--- say-hi.ts
+++ say-hi.ts
@@ -1,9 +1,19 @@
 import { message, warn, danger, markdown } from "danger"
 
 export default async () => {
-    const issue = danger.github.issue;
+    const gh = danger.github as any;
+    const repo = gh.repository;
+    const issue = gh.issue;
+
     console.log('Dealing with an issue here', { issue });
     message("Hi!");
     warn('Uh-oh, that is not good');
     markdown('# This does not work, does it?');
+
+    await danger.github.api.issues.addLabels({
+        owner: repo.owner.login,
+        repo: repo.name,
+        number: issue.number,
+        labels: ["good first issue"]
+    });
 }
