403029973acfbeb60778db2aa3ef96ba51fd9ebe
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-22 16:07:53 -0700

    Setup prettier

--- tests/checkDescriptionLength.test.ts
+++ tests/checkDescriptionLength.test.ts
@@ -2,36 +2,37 @@ jest.mock("danger", () => jest.fn());
 import * as danger from "danger";
 const dm = danger as any;
 
-import {checkDescriptionLength} from '../orgs/checkDescriptionLength'
+import { checkDescriptionLength } from "../orgs/checkDescriptionLength";
 
-it('emits a warning when the length of the pull-request is too small', () => {
-    dm.warn = jest.fn();
+it("emits a warning when the length of the pull-request is too small", () => {
+  dm.warn = jest.fn();
 
-    dm.danger = {
-        github: {
-        pr: {
-            body: 'boop'
-        }
+  dm.danger = {
+    github: {
+      pr: {
+        body: "boop"
+      }
     }
-    };
+  };
 
-    return checkDescriptionLength().then(() => {
-        expect(dm.warn).toHaveBeenCalledWith('The description is very short.')
-    })
+  return checkDescriptionLength().then(() => {
+    expect(dm.warn).toHaveBeenCalledWith("The description is very short.");
+  });
 });
 
-it('does not say anything when the body of the pr is big enough', () => {
-    dm.warn = jest.fn();
+it("does not say anything when the body of the pr is big enough", () => {
+  dm.warn = jest.fn();
 
-    dm.danger = {
-        github: {
-        pr: {
-            body: 'This pull request fixes a bug that we had with the thing that does something important.'
-        }
+  dm.danger = {
+    github: {
+      pr: {
+        body:
+          "This pull request fixes a bug that we had with the thing that does something important."
+      }
     }
-    };
+  };
 
-    return checkDescriptionLength().then(() => {
-        expect(dm.warn).not.toHaveBeenCalled();
-    })
+  return checkDescriptionLength().then(() => {
+    expect(dm.warn).not.toHaveBeenCalled();
+  });
 });
